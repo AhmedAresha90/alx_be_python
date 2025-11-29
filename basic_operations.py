378a3ff8db1bdcda5b645a4da1473aceeb3f823e
unknown <ahmedphysical90@gmail.com>
2025-11-29 20:29:05 +0800

    basic_operayions.py file

--- basic_operations.py
+++ basic_operations.py
@@ -1,12 +1,12 @@
 number1 = 10
 number2 = 5
 
-# Calculations
+# Calculations1
 sum_result = number1 + number2
 difference_result = number1 - number2
 product_result = number1 * number2
 
-# Printing results
+# Printing results1
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
