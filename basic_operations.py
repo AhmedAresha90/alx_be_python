1aa8234846f495fb78c41c3f3443813012b74f63
unknown <ahmedphysical90@gmail.com>
2025-11-29 19:19:51 +0800

    modify python file

--- basic_operations.py
+++ basic_operations.py
@@ -1,9 +1,11 @@
 number1 = 10
 number2 = 5
+
 # Calculations
 sum_result = number1 + number2
 difference_result = number1 - number2
 product_result = number1 * number2
+
 # Printing results
 print(f"Sum of {number1} and {number2} is {sum_result}")
 print(f"Difference of {number1} and {number2} is {difference_result}")
