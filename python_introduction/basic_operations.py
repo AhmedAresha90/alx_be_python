928757f0e1ecb4ce6d15f6565e387dd7c7ef3e1c
unknown <ahmedphysical90@gmail.com>
2025-11-30 01:41:30 +0800

    python introduction folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+number1 = 10
+number2 = 5
+
+# Calculations1
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Printing results1
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+=======
+if __name__ == "__main__":
+    number1 = 10
+    number2 = 5
+
+    sum_result = number1 + number2
+    difference_result = number1 - number2
+    product_result = number1 * number2
+
+    print(f"Addition of {number1} and {number2} is {sum_result}")
+    print(f"Subtraction of {number1} and {number2} is {difference_result}")
+    print(f"Multiplication of {number1} and {number2} is {product_result}")
+>>>>>>> 206fbf40de1a25b7bd550e63eea278da98604d20
