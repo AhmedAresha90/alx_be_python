b71f5927d761e8cd0c795c778dd41e6b66ab2ff5
AhmedAresha90 <Ahmedphysical90@gmail.com>
2025-12-04 04:01:47 +0800

    basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -2,25 +2,11 @@
 number1 = 10
 number2 = 5
 
-# Calculations1
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 
-# Printing results1
-print(f"Addition of {number1} and {number2} is {sum_result}")
-print(f"Subtraction of {number1} and {number2} is {difference_result}")
-print(f"Multiplication of {number1} and {number2} is {product_result}")
-=======
-if __name__ == "__main__":
-    number1 = 10
-    number2 = 5
-
-    sum_result = number1 + number2
-    difference_result = number1 - number2
-    product_result = number1 * number2
-
-    print(f"Addition of {number1} and {number2} is {sum_result}")
-    print(f"Subtraction of {number1} and {number2} is {difference_result}")
-    print(f"Multiplication of {number1} and {number2} is {product_result}")
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number2} and {number1} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
 >>>>>>> 206fbf40de1a25b7bd550e63eea278da98604d20
