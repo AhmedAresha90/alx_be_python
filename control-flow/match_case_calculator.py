5e316e2093abab54ab4febfd4e2b5a532d4bfdeb
AhmedAresha90 <Ahmedphysical90@gmail.com>
2025-12-05 01:19:30 +0800

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,8 +1,8 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operations = input("Choose the operation (+, -, *, /): ")
-
 match operations:
+
     case "+":
         print(f"The result is {num1 + num2}")
     case "-":
