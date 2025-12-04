73997100f2209021d7473302ef69547818ee0262
AhmedAresha90 <Ahmedphysical90@gmail.com>
2025-12-05 01:03:39 +0800

    match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,18 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 op = input("Choose the operation (+, -, *, /): ")
-match op :
-    case "+" :
+match op:
+    case "+":
         print(f"the result is {num1 + num2}")
-    case "-" :
+    case "-":
         print(f"the result is {num1 - num2}")
-    case "*" :
+    case "*":
         print(f"the result is {num1 * num2}")
-    case "/" :
+    case "/":
         if num2 != 0:
             print(f"the result is {num1 / num2}")
         else: 
             print("Cannot divide by zero.")
-    case _ :
+    case _:
         print("unknown operator")
       
